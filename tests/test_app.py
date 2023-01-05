0f678aded74b10d6e24bfa148d04e9eb5d47f9ee
Andrey Ivanov <ivnv.xd@gmail.com>
2023-01-05 14:18:12 +0100

    add urls pages

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,5 +5,14 @@ def test_index_route():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    # assert response.data.decode('utf-8') == 'Welcome!'
-    assert b"Welcome" in response.data
+    assert b"Page Analyzer" in response.data
+
+
+def test_urls_route():
+    response = app.test_client().get('/urls')
+    assert response.status_code == 200
+
+
+def test_random_url_route():
+    response = app.test_client().get('/urls/1')
+    assert response.status_code == 200
