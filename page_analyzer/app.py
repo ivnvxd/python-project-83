3e4b865082e61c9d3abe65bcbd01019a8eb713b8
Andrey Ivanov <ivnv.xd@gmail.com>
2023-01-05 14:17:59 +0100

    add input form

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,3 +8,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/urls')
+def urls_get():
+    return render_template('urls.html')
+
+
+@app.route('/urls/<int:id>')
+def url_show(id):
+    return render_template(
+        'show.html',
+        id=id
+    )
