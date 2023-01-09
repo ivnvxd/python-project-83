46530766e781a0aa7f55d36659e812a2a56dcf4b
Andrey Ivanov <ivnv.xd@gmail.com>
2023-01-09 15:15:56 +0100

    add tests without db check

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,31 @@
+import pytest
 from page_analyzer.app import app
 
 
-def test_index_route():
-    response = app.test_client().get('/')
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    yield client
+
+
+def test_index_route(client):
+    response = client.get('/')
+    html = response.data.decode()
+
     assert response.status_code == 200
-    # assert b"Анализатор страниц" in response.data
+
+    assert '<p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>' in html
+
+    assert '<a class="navbar-brand" href="/">Анализатор страниц</a>' in html
+    assert '<a class="nav-link " href="/urls">Сайты</a>' in html
 
 
-# def test_urls_route():
-#     response = app.test_client().get('/urls')
-#     assert response.status_code == 200
-#
-#
-# def test_random_url_route():
-#     response = app.test_client().get('/urls/1')
-#     assert response.status_code == 200
+def test_404(client):
+    response = client.get('/qwerty')
+    html = response.data.decode()
+
+    assert response.status_code == 404
+
+    assert '<h1>Страница не найдена</h1>' in html
---
+++ tests/test_checks.py
@@ -0,0 +1,40 @@
+import pytest
+from requests import RequestException
+from page_analyzer.checks import validate_url, get_url_data
+
+
+@pytest.fixture()
+def site():
+    urls = {}
+    urls['zero'] = {'url': '', 'error': 'zero'}
+    urls['invalid'] = {'url': 'url.com', 'error': 'invalid'}
+    urls['long'] = {'url': 'https://www.google.com/1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890', 'error': 'length'}
+    urls['valid'] = {'url': 'https://devguide.python.org/documentation/style-guide/#style-guide', 'error': ''}
+
+    return urls
+
+
+@pytest.fixture()
+def check():
+    checks = {}
+    checks['right'] = 'https://www.google.com/'
+    checks['wrong'] = 'http://wrong.com'
+
+    return checks
+
+
+def test_validation_errors(site):
+    assert validate_url(site['zero']['url']) == {'url': site['zero']['url'], 'error': 'zero'}
+    assert validate_url(site['invalid']['url']) == {'url': site['invalid']['url'], 'error': 'invalid'}
+    assert validate_url(site['long']['url']) == {'url': site['long']['url'], 'error': 'length'}
+
+    # valid = 'https://devguide.python.org'
+    # assert validate_url(site['valid']['url']) == {'url': valid, 'error': ''}
+
+
+def test_check(check):
+    right = get_url_data(check['right'])
+    assert right == {'description': '', 'h1': '', 'status_code': 200, 'title': 'Google'}
+
+    with pytest.raises(RequestException):
+        get_url_data(check['wrong'])
